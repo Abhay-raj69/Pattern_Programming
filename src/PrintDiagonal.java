e66e5c48d6934173d260ec0a691ea757e3dca2b0
Abhay-raj69 <rajabhay56999@gmail.com>
2023-08-08 02:01:49 +0530

    again added

---
+++ src/PrintDiagonal.java
@@ -0,0 +1,28 @@
+/*
+Print this
+ *
+   *
+     *
+       *
+         *
+
+ */
+import java.util.Scanner;
+public class PrintDiagonal {
+    public static void main(String[] args){
+        Scanner pk=new Scanner(System.in);
+        System.out.print("Enter the value of n : ");
+        int n=pk.nextInt();
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n;j++){
+                if (i==j){
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/PrintX.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class PrintX {
+    public static void main(String[] args){
+        Scanner pk=new Scanner(System.in);
+        System.out.print("Enter the value of n : ");
+        int n=pk.nextInt();
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n;j++){
+                if (i==j || i+j==n-1){
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
